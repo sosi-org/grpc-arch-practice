14b400df8a50e96e89985cae2e49d43a163b4888
Sohail Siadatnejad <sohale@gmail.com>
2020-02-16 00:37:50 +0000

    prot.js + .proto

---
+++ example/prot.js
@@ -0,0 +1,17 @@
+var PROTO_PATH = //__dirname + '/../../../protos/route_guide.proto';
+	__dirname + '/serv1.proto';
+var grpc = require('grpc');
+var protoLoader = require('@grpc/proto-loader');
+// Suggested options for similarity to existing grpc.load behavior
+var packageDefinition = protoLoader.loadSync(
+    PROTO_PATH,
+    {keepCase: true,
+     longs: String,
+     enums: String,
+     defaults: true,
+     oneofs: true
+    });
+var protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
+// The protoDescriptor object has the full package hierarchy
+var routeguide = protoDescriptor.routeguide;
+
