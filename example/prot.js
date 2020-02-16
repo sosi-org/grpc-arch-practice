4cf45deaa04f768d1947b42d12f5977557860644
Sohail Siadatnejad <sohale@gmail.com>
2020-02-16 00:55:05 +0000

    Numbers service: to practice grpc streams and services

--- example/prot.js
+++ example/prot.js
@@ -1,6 +1,7 @@
 var PROTO_PATH = //__dirname + '/../../../protos/route_guide.proto';
 	__dirname + '/serv1.proto';
-var grpc = require('grpc');
+
+    var grpc = require('grpc');
 var protoLoader = require('@grpc/proto-loader');
 // Suggested options for similarity to existing grpc.load behavior
 var packageDefinition = protoLoader.loadSync(
@@ -13,5 +14,15 @@ var packageDefinition = protoLoader.loadSync(
     });
 var protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
 // The protoDescriptor object has the full package hierarchy
-var routeguide = protoDescriptor.routeguide;
+
+var numbersService = protoDescriptor.NumbersService;
+console.log(protoDescriptor);
+console.log(numbersService);
+
+/*
+NumbersService: { ToStr, GetNextNumbers, GenerateStrings, AddNumbers }
+Number
+NumStr
+*/
+
 
