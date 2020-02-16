fec3212bb313fcf228048354cb7fb880583830e2
Sohail Siadatnejad <sohale@gmail.com>
2020-02-16 01:06:08 +0000

    proper client: still not working. no server

--- example/prot-client.js
+++ example/prot-client.js
@@ -1,3 +1,9 @@
+/*
+Based on https://github.com/grpc/grpc/blob/v1.27.0/examples/node/dynamic_codegen/route_guide/route_guide_client.js
+*/
+
+var async = require('async');
+
 var PROTO_PATH = //__dirname + '/../../../protos/route_guide.proto';
 	__dirname + '/serv1.proto';
 
@@ -16,8 +22,10 @@ var protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
 // The protoDescriptor object has the full package hierarchy
 
 var numbersService = protoDescriptor.NumbersService;
+/*
 console.log(protoDescriptor);
 console.log(numbersService);
+*/
 
 /*
 NumbersService: { ToStr, GetNextNumbers, GenerateStrings, AddNumbers }
@@ -26,3 +34,9 @@ NumStr
 */
 
 
+const SERVER_ADDRESS = 'localhost:50051';
+
+var client = new numbersService.NumbersService(SERVER_ADDRESS, grpc.credentials.createInsecure());
+
+console.log('client', client);
+console.log('okk')
