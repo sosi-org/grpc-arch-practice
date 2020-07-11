904d10e957f8d5cc2e6a9a04317ba4891de198a1
Sohail Siadatnejad <sohale@gmail.com>
2020-07-11 23:52:42 +0100

    load dataset: mnist/fashion

--- tfserving-example/tf-model-create.py
+++ tfserving-example/tf-model-create.py
@@ -14,3 +14,27 @@ import subprocess
 
 print('TensorFlow version: {}'.format(tf.__version__))
 # TensorFlow version: 2.2.0
+# ----- tf version settled -----------------
+
+print('Loading fashion-mnist dataset')
+
+fashion_mnist = keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+
+# scale the values to 0.0 to 1.0
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+# reshape for feeding into the model
+train_images = train_images.reshape(train_images.shape[0], 28, 28, 1)
+test_images = test_images.reshape(test_images.shape[0], 28, 28, 1)
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+print('\ntrain_images.shape: {}, of {}'.format(train_images.shape, train_images.dtype))
+print('test_images.shape: {}, of {}'.format(test_images.shape, test_images.dtype))
+
+
+# Train and evaluate the model
