e0e37a4862b636fc3393ae23a4aa8825193ebde8
Sohail Siadatnejad <sohale@gmail.com>
2020-07-12 00:29:53 +0100

    indent

--- tfserving-example/tf-model-create.py
+++ tfserving-example/tf-model-create.py
@@ -17,70 +17,74 @@ print('TensorFlow version: {}'.format(tf.__version__))
 # TensorFlow version: 2.2.0
 # ----- tf version settled -----------------
 
-#def load_dataset():
-print('Loading fashion-mnist dataset')
+def load_dataset():
+    print('Loading fashion-mnist dataset')
 
-fashion_mnist = keras.datasets.fashion_mnist
-(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+    fashion_mnist = keras.datasets.fashion_mnist
+    (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
 
-# scale the values to 0.0 to 1.0
-train_images = train_images / 255.0
-test_images = test_images / 255.0
+    # scale the values to 0.0 to 1.0
+    train_images = train_images / 255.0
+    test_images = test_images / 255.0
 
-# reshape for feeding into the model
-train_images = train_images.reshape(train_images.shape[0], 28, 28, 1)
-test_images = test_images.reshape(test_images.shape[0], 28, 28, 1)
+    # reshape for feeding into the model
+    train_images = train_images.reshape(train_images.shape[0], 28, 28, 1)
+    test_images = test_images.reshape(test_images.shape[0], 28, 28, 1)
 
-class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
-               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+    class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+                  'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
-print('\ntrain_images.shape: {}, of {}'.format(train_images.shape, train_images.dtype))
-print('test_images.shape: {}, of {}'.format(test_images.shape, test_images.dtype))
+    print('\ntrain_images.shape: {}, of {}'.format(train_images.shape, train_images.dtype))
+    print('test_images.shape: {}, of {}'.format(test_images.shape, test_images.dtype))
 
+    return train_images, train_labels, test_images, test_labels, class_names,
 
 # Train and evaluate the model
-model = keras.Sequential([
-  keras.layers.Conv2D(input_shape=(28,28,1), filters=8, kernel_size=3,
-                      strides=2, activation='relu', name='Conv1'),
-  keras.layers.Flatten(),
-  keras.layers.Dense(10, activation=tf.nn.softmax, name='Softmax')
-])
-model.summary()
+def train_eval(train_images, train_labels, test_images, test_labels):
+    model = keras.Sequential([
+      keras.layers.Conv2D(input_shape=(28,28,1), filters=8, kernel_size=3,
+                          strides=2, activation='relu', name='Conv1'),
+      keras.layers.Flatten(),
+      keras.layers.Dense(10, activation=tf.nn.softmax, name='Softmax')
+    ])
+    model.summary()
 
-testing = False
-epochs = 5
+    testing = False
+    epochs = 5
 
-model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
-model.fit(train_images, train_labels, epochs=epochs)
+    model.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+    model.fit(train_images, train_labels, epochs=epochs)
 
-test_loss, test_acc = model.evaluate(test_images, test_labels)
-print('\nTest accuracy: {}'.format(test_acc))
+    test_loss, test_acc = model.evaluate(test_images, test_labels)
+    print('\nTest accuracy: {}'.format(test_acc))
+
+    return model
 
 
-def save_model():
-  # Fetch the Keras session and save the model
-  # The signature definition is defined by the input and output tensors,
-  # and stored with the default serving key
-  import tempfile
+def save_model(model):
+    # Fetch the Keras session and save the model
+    # The signature definition is defined by the input and output tensors,
+    # and stored with the default serving key
+    import tempfile
 
-  MODEL_DIR = tempfile.gettempdir()
-  version = 1
-  export_path = os.path.join(MODEL_DIR, str(version))
-  print('export_path = {}\n'.format(export_path))
+    MODEL_DIR = tempfile.gettempdir()
+    version = 1
+    export_path = os.path.join(MODEL_DIR, str(version))
+    print('export_path = {}\n'.format(export_path))
 
-  tf.keras.models.save_model(
-      model,
-      export_path,
-      overwrite=True,
-      include_optimizer=True,
-      save_format=None,
-      signatures=None,
-      options=None
-  )
+    tf.keras.models.save_model(
+        model,
+        export_path,
+        overwrite=True,
+        include_optimizer=True,
+        save_format=None,
+        signatures=None,
+        options=None
+    )
 
-  print('\nSaved model:')
-  #!ls -l {export_path}
-  sh(f'ls -l {export_path}')
+    print('\nSaved model:')
+    #!ls -l {export_path}
+    sh(f'ls -l {export_path}')
