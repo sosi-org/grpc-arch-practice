a51adbfaea3daf8e162c212120609fe572708a5c
Sohail Siadatnejad <sohale@gmail.com>
2020-07-12 01:50:54 +0100

    running server inside the docker works

--- tfserving-example/serve-process.sh
+++ tfserving-example/serve-process.sh
@@ -6,6 +6,8 @@ apt update -y
 apt-get install curl -y
 curl --version
 
+apt-get install -y gnupg
+
 echo "SERVER PROCESS:"
 # This is the same as you would do from your command line, but without the [arch=amd64], and no sudo
 # You would instead do:
@@ -14,10 +16,10 @@ echo "SERVER PROCESS:"
 
 echo "deb http://storage.googleapis.com/tensorflow-serving-apt stable tensorflow-model-server tensorflow-model-server-universal" | tee /etc/apt/sources.list.d/tensorflow-serving.list && \
 curl https://storage.googleapis.com/tensorflow-serving-apt/tensorflow-serving.release.pub.gpg | apt-key add -
-# apt update
+apt update -y
 
 # install the tf serving
-apt-get install -y tensorflow-model-server
+apt-get install tensorflow-model-server
 
 # os.environ["MODEL_DIR"] = MODEL_DIR
 # export MODEL_DIR=/var/folders/5g/kz1p_241503bfrqndt8qy7640000gn/T/1
@@ -26,10 +28,13 @@ apt-get install -y tensorflow-model-server
 
 echo "MODEL_DIR: $MODEL_DIR"
 
+mkdir -p /tfserve1/runtime
+
 # %%bash --bg
-nohup tensorflow_model_server \
+#nohup
+tensorflow_model_server \
   --rest_api_port=8501 \
   --model_name=fashion_model \
-  --model_base_path="$MODEL_DIR" >server.log 2>&1
+  --model_base_path="$MODEL_DIR" >/tfserve1/runtime/server.log 2>&1
 
 # tail server.log
