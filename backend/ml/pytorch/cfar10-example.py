aab12c2a53b63a00a378b680bd51432a485d84b2
Sohail Siadatnejad <sohale@gmail.com>
2020-08-25 19:30:52 +0100

    pytorch script based on cfar tutorial

---
+++ backend/ml/pytorch/cfar10-example.py
@@ -0,0 +1,163 @@
+import torch.nn.functional as F
+import torch.nn as nn
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+SAVE_PATH = './my-cifar_net.pth'
+
+
+def imshow(img):
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
+def install_cfar10():
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    batch_size = 4
+
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                            download=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                              shuffle=True, num_workers=2)
+
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                           download=True, transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                             shuffle=False, num_workers=2)
+
+    classes = ('plane', 'car', 'bird', 'cat',
+               'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    # functions to show an image
+
+    a_few = False
+    if a_few:
+        # get some random training images
+        dataiter = iter(trainloader)
+        images, labels = next(dataiter)
+
+        # show images
+        imshow(torchvision.utils.make_grid(images))
+        # print labels
+        print(' '.join(f'{classes[labels[j]]:5s}' for j in range(batch_size)))
+
+    # return trainloader, testloader, classes
+    return trainloader, testloader, classes
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1)  # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+# net = Net()
+# print(net)
+
+
+def nn_exerpiment1(trainloader, testloader, classes):
+
+    net = Net()
+    try:
+        net.load_state_dict(torch.load(SAVE_PATH))
+    except e:
+        print("loading saved state failed. Continuing without loading")
+
+    import torch.optim as optim
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+    # train:
+    for epoch in range(2):  # loop over the dataset multiple times
+
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+            if i % 2000 == 1999:    # print every 2000 mini-batches
+                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+                running_loss = 0.0
+            if i > 4000:
+                break
+        print('.')
+    print(':')
+
+    print('Finished Training')
+
+    torch.save(net.state_dict(), SAVE_PATH)
+    print('Saved state')
+
+    # test:
+    dataiter = iter(testloader)
+    images, labels = next(dataiter)
+
+    # print images
+    imshow(torchvision.utils.make_grid(images))
+
+    print('GroundTruth: ', ' '.join(
+        f'{classes[labels[j]]:5s}' for j in range(4)))
+
+
+def load_and_1(trainloader, testloader, classes):
+    dataiter = iter(testloader)
+    images, labels = next(dataiter)
+
+    print('GroundTruth: ', ' '.join(
+        f'{classes[labels[j]]:5s}' for j in range(4)))
+
+    net = Net()
+    net.load_state_dict(torch.load(SAVE_PATH))
+
+    outputs = net(images)
+
+    _, predicted = torch.max(outputs, 1)
+
+    print('Predicted: ', ' '.join(f'{classes[predicted[j]]:5s}'
+          for j in range(4)))
+
+
+if __name__ == '__main__':
+    trainloader, testloader, classes = install_cfar10()
+    #  # show_some_demo()
+    #  nn_exerpiment1(trainloader, testloader, classes)
+    load_and_1(trainloader, testloader, classes)
+
+
+"""
+[1] Wonderful tutorial: https://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html
+"""
