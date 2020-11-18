180316e116d28e9f67abe36c8d58cca7ba8bac40
Sohail Siadatnejad <sohale@gmail.com>
2020-11-18 23:07:25 +0000

    collecting commaans for the gist of the previous attempt

---
+++ nov-2020/init.sh
@@ -0,0 +1,26 @@
+
+# This is my attempt to rememerb things I did on 4 October 2020.
+# However, was it based doocker??
+# I need to turn my model into this format.
+# now: 18 Nov 2020 23:02
+# . ~/core/env-me-mine.sh
+
+# from: /Users/a9858770/cs/ml--nn/grpc-arch-practice/tf-serving-18-sept-2020/prepare-env.sh
+export GAP=~/cs/ml--nn/grpc-arch-practice
+export VENV=~/cs/ml--nn/grpc-arch-practice/tf2b
+cd $GAP
+source $VENV/bin/activate
+
+# cd ~/cs/ml--nn/grpc-arch-practice/nov-2020
+
+if false; then
+
+  ln -s ~/cs/ml--nn/grpc-arch-practice/tf-serving-18-sept-2020/dl/tmp/resnet /tmp
+
+  ls /tmp/resnet/resnet_client.py
+
+  docker run -p 8501:8501 --name tfserving_resnet --mount type=bind,source=/tmp/resnet,target=/models/resnet -e MODEL_NAME=resnet -t tensorflow/serving &
+
+  python /tmp/resnet/resnet_client.py
+
+fi
---
+++ nov-2020/notes.md
@@ -0,0 +1,12 @@
+This seems to be the successful attempt back from October
+
+copying 'resnet_client.py' from:
+cd ~/cs/ml--nn/grpc-arch-practice/nov-2020
+cp ../tf-serving-18-sept-2020/dl/tmp/resnet/resnet_client.py .
+(See link [1] below)
+
+Seems to be basaed on:
+[1] https://medium.com/tensorflow/serving-ml-quickly-with-tensorflow-serving-and-docker-7df7094aa008
+
+Also this may be relevant:
+[2] https://github.com/tensorflow/serving/blob/master/tensorflow_serving/g3doc/docker.md
